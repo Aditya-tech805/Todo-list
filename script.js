ae48fb8dfac39e73671f4471416c21ef0785a521
SwaggerAditya@ <Adityapathak2596@gmail.com>
2025-04-08 23:22:44 +0530

    Update script.js

--- script.js
+++ script.js
@@ -11,23 +11,86 @@ function addTodo() {
 
     const todoitem = document.createElement("li");
 
-    const checkbox = document.createElement("input");
-    checkbox.type = "checkbox";
-    checkbox.addEventListener("change", function () {
-        if (this.checked) {
-            tododiv.style.textDecoration = "line-through";
-        } else {
-            tododiv.style.textDecoration = "none";
-        }
-    });
-
     const tododiv = document.createElement("div");
     tododiv.innerText = todotext;
     tododiv.style.display = "inline";
 
+     // here in this function actually i am want to make my tododiv editable 
+    tododiv.addEventListener("click",function(){
+    const currentText=tododiv.innerText;
+
+    const input =document.createElement("input");
+    input.type="text";
+    input.value=currentText;
+    input.style.marginRight= "10px";
+
+    tododiv.replaceWith(input);
+    input.focus();
+
+    function saveEdit() {
+        const newText = input.value.trim();
+        if (newText !== "") {
+            tododiv.innerText = newText;
+        }
+        input.replaceWith(tododiv);
+    }
+
+    input.addEventListener("blur", saveEdit);
+
+    input.addEventListener("keydown", function (event) {
+        if (event.key === "Enter") {
+            saveEdit();
+        }
+    });
+  });
+
+  const checkbox = document.createElement("input");
+  checkbox.type = "checkbox";
+  checkbox.addEventListener("change", function () {
+      if (this.checked) {
+          tododiv.style.textDecoration = "line-through";
+      } else {
+          tododiv.style.textDecoration = "none";
+      }
+  });
+
     const deletebtn = document.createElement("button");
     deletebtn.innerText = "Delete";
     deletebtn.style.background = "red";
+
+    const Edittbtn =document.createElement("button");
+    Edittbtn.innerText ="Edit";
+    Edittbtn.style.background="red";
+
+
+    Edittbtn.addEventListener("click", function ( ){
+        const currentText = tododiv.innerText;
+
+        const input = document.createElement("input");
+        input.type = "text";
+        input.value = currentText;
+        input.style.marginRight = "10px";
+    
+        tododiv.replaceWith(input);
+        input.focus();
+    
+        // Save on blur or Enter key
+        function saveEdit() {
+            const newText = input.value.trim();
+            if (newText !== "") {
+                tododiv.innerText = newText;
+            }
+            input.replaceWith(tododiv);
+        }
+    
+        input.addEventListener("blur", saveEdit);
+        input.addEventListener("keydown", function (event) {
+            if (event.key === "Enter") {
+                saveEdit();
+            }
+        });
+    });
+
 
     deletebtn.addEventListener("click", function () {
         deletebtn.parentElement.remove();
@@ -36,6 +99,7 @@ function addTodo() {
     todoitem.appendChild(checkbox);
     todoitem.appendChild(tododiv);
     todoitem.appendChild(deletebtn);
+    todoitem.appendChild(Edittbtn);
 
     todocant.append(todoitem);
     textarea.value = "";
@@ -49,4 +113,15 @@ textarea.addEventListener("keydown", function (event) {
     if (event.key === "Enter") {
         addTodo();
     }
+
 });
+// this function help to save the todo content whenever i click on the page 
+textarea.addEventListener("blur", function(){
+    if(textarea.value.trim() !==""){
+    addTodo();
+    }
+
+
+});   
+
+
